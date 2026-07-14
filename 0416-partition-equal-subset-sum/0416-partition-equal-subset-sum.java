af606514ac46560355023992e802019ff994afd2
Naveen032006 <naveenkumar03202006@gmail.com>
2026-07-15 02:03:41 +0530

    Time: 42 ms (61.14%), Space: 72.2 MB (34.99%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -19,9 +19,22 @@ class Solution {
         if(sum%2!=0) return false;
          int[][] dp=new int[n][sum/2+1];
          for(int[] row:dp){
-            Arrays.fill(row,-1);
+            Arrays.fill(row,0);
          }
+         for(int i=0;i<n;i++){
+            dp[i][0]=1;
+         }
+        if(nums[0]<=sum/2)dp[0][nums[0]]=1;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=sum/2;j++){
+                 int nopick=dp[i-1][j];
+                  int pick=0;
+                  if(j>=nums[i]) pick=dp[i-1][j-nums[i]];
+                  dp[i][j]=Math.max(pick,nopick);
+            }
+        }
         
-        return solve(nums,n-1,sum/2,dp);
+
+        return dp[n-1][sum/2]==1;
     }
 }
