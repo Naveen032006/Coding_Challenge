7015854b90082bdf71c88c531a7c1dd8c1ae178a
Naveen032006 <naveenkumar03202006@gmail.com>
2026-08-17 18:57:35 +0530

    Time: 0 ms (100%), Space: 43.1 MB (98.03%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solve(int[][] arr,int i,int j,int[][] dp){
+        if(i==0&&j==0) return 1;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int top=0;
+        if(i>0&&arr[i][j]!=1) top=solve(arr,i-1,j,dp);
+        int left=0;
+        if(j>0&&arr[i][j]!=1) left=solve(arr,i,j-1,dp);
+
+        return dp[i][j]=left+top;
+    }
+    public int uniquePathsWithObstacles(int[][] arr) {
+        int n=arr.length;
+        int m=arr[0].length;
+        if(arr[0][0]==1) return 0;
+        if(arr[n-1][m-1]==1) return 0;
+        int[][] dp=new int[n][m];
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        return solve(arr,n-1,m-1,dp);
+    }
+}
