9e98b126538464953cb493ee27ba5fabe2406ce2
Naveen032006 <naveenkumar03202006@gmail.com>
2026-08-23 12:13:04 +0530

    Time: 10 ms (12.02%), Space: 47 MB (78.12%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,34 +1,40 @@
 class Solution {
-    public boolean dfs(int i,ArrayList<ArrayList<Integer>> adj,int[] vis,int[] path){
-        vis[i]=1;
-        path[i]=1;
-        for(int node:adj.get(i)){
-            if(vis[node]!=1){
-                if(dfs(node,adj,vis,path)) return true;
-            }
-            else if(path[node]==1) return true;
-        }
-        path[i]=0;
-        return false;
-        
-    }
     public boolean canFinish(int n, int[][] pre) {
-        int m =pre.length;
+        int m=pre.length;
+        int[] indegree=new int[n];
         ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        ArrayList<Integer> ans=new ArrayList<>();
+        Queue<Integer> q=new LinkedList<>();
         for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        int[] vis=new int[n];
-        int[] path=new int[n];
         for(int i=0;i<m;i++){
             adj.get(pre[i][1]).add(pre[i][0]);
         }
         for(int i=0;i<n;i++){
-            if(vis[i]!=1){
-                if(dfs(i,adj,vis,path)) return false;
+            for(int node:adj.get(i)){
+                indegree[node]++;
             }
         }
-        return true;
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.add(i);
+                count++;
+            } 
+        }
+        
+        while(!q.isEmpty()){
+            int node=q.poll();
+            for(int adjnode:adj.get(node)){
+                indegree[adjnode]--;
+                if(indegree[adjnode]==0){
+                    q.add(adjnode);
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+        return count==n;
+
     }
 }
